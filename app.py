324b63d1be5e73d5c9fa6f52da7cdac13d124aea
Tarun <tarunashwini23@gmail.com>
2024-06-24 16:20:40 +0530

    Revert "Conflict occured during  reverting the commit"
    This reverts commit 502c94ee3209c1096399eb864b79489f46be0bdb.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 print("Hello World! This is assignment_Repo1. I have also made changes using github.")
 print("I have edited this part after I have occured with conflict")
 print("This is general commit")
-print("Conflit occured during the revert")
+
