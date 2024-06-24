44dad9d0e87d5bc54b3313c44fc21ad4ac062e72
Tarun <tarunashwini23@gmail.com>
2024-06-24 14:59:41 +0530

    Commit after occuring merge conflict

--- app.py
+++ app.py
@@ -1,1 +1,3 @@
-print("Hello World! This is assignment_Repo1. I have also made changes using github")
+print("Hello World! This is assignment_Repo1. I have also made changes using github.")
+print("I have edited this part after I have occured with conflict")
+
